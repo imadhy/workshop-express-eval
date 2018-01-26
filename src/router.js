ba53368f0721ec15fa0f2b8e0a7af602b26441ef
imadhy <imadhy.gt@gmail.com>
2018-01-26 15:53:26 +0100

    Exercice 5

--- src/router.js
+++ src/router.js
@@ -3,12 +3,40 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/bingo', function (req, res) {
+
     fs.readFile('./src/numbers.txt', "utf8", function read(err, data) {
+
+        numbers = data.split('\n');
+        printNumbers = data.split('\n').join(',');
+
         if (err) {
             throw err;
         }
-        numbers = data.split('\n').join(',');
-        res.send(numbers);
+
+        if (req.query.mynumbers) {
+            receivedNumbers = req.query.mynumbers.split(',');
+            let bingo = true;
+
+            if (receivedNumbers.length < 5) {
+                bingo = false;
+            } else {
+                for (let v of receivedNumbers) {
+                    if (!numbers.includes(v)) {
+                        bingo = false;
+                        break;
+                    }
+                }
+            }
+
+            if (bingo) {
+                res.send('Bingo');
+            } else {
+                res.send(`The bingo game is already started, sorry your numbers doesn't match with known numbers ${printNumbers} ; so you can not say Bingo`);
+            }
+
+        } else {
+            res.send(`The bingo game is already started and known numbers are ${printNumbers}`);
+        }
     });
 });
 
