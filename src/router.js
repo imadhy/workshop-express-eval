678868ac8db1d4c26548539e1fac6711a91acc37
imadhy <imadhy.gt@gmail.com>
2018-01-26 15:26:46 +0100

    Exercice 4

--- src/router.js
+++ src/router.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/bingo', function (req, res) {
-    res.send('Bingo');
+    fs.readFile('./src/numbers.txt', "utf8", function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        numbers = data.split('\n').join(',');
+        res.send(numbers);
+    });
 });
 
 module.exports = router;
